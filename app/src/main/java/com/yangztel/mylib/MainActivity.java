98d4876e916dc2194d76007b02c49dfe694381b3
yangzteL <965579738@qq.com>
2018-07-24 10:07:40 +0800

    沉浸式状态栏

--- app/src/main/java/com/yangztel/mylib/MainActivity.java
+++ app/src/main/java/com/yangztel/mylib/MainActivity.java
@@ -1,42 +1,95 @@
 package com.yangztel.mylib;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.net.http.SslError;
 import android.os.Build;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.webkit.JsResult;
 import android.webkit.SslErrorHandler;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
 
 import com.yangztel.lbase.mvp.RxBus;
 import com.yangztel.lbase.mvp.RxManager;
+import com.yangztel.mylib.util.StatusBarCompat;
 
 import io.reactivex.Observable;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
     RxManager rxManager = new RxManager();
     private WebView wvHelp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+//5.0及以上系统才支持
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            View decorView = getWindow().getDecorView();
+//两个Flag必须要结合在一起使用，表示会让应用的主体内容占用系统状态栏的空间
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+            decorView.setSystemUiVisibility(option);
+//将状态栏设置成透明色
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+//        ActionBar actionBar = getSupportActionBar();
+//        if(actionBar != null){
+//            //将actionBar隐藏
+//            actionBar.hide();
+//        }
+
+
+//        StatusBarCompat.setStatusBarColor(this, ContextCompat.getColor(this,R.color.black));
+
+
         wvHelp = findViewById(R.id.wv_w);
 
         initWebView("http://182.150.20.24:10087/ZHHWeb/H5/about/about.html");
 
         rxbus();
     }
+
+
+
+    private void initWindows() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+            window.setNavigationBarColor(Color.BLACK);
+        }
+    }
+
     @SuppressLint("JavascriptInterface")
     public void initWebView(String url) {
         WebSettings settings = wvHelp.getSettings();
